c2133f23dab5de744f2c9d2fb8da4e5151267760
luis <luisaguilar098@gmail.com>
2018-10-24 17:48:11 -0600

    Created simple addition program.

---
+++ practice.py
@@ -0,0 +1,14 @@
+#Practice in Python
+
+import math
+
+print("Enter the first number")
+a = input()
+print("Enter the second number")
+b = input()
+c = a + b
+ans = "{} + {} = {}".format(a,b,c)
+print(ans)
+
+
+
