78e47563bd0015cfbe27bfbe6c222d4a82932306
luis <luisaguilar098@gmail.com>
2018-10-24 18:08:25 -0600

    added math function

--- practice.py
+++ practice.py
@@ -1,14 +1,17 @@
 #Practice in Python
 
 import math
+a = "" 
+b = ""
+c = ""
 
-print("Enter the first number")
-a = input()
-print("Enter the second number")
-b = input()
-c = a + b
-ans = "{} + {} = {}".format(a,b,c)
-print(ans)
+def math(a,b,c):
+    print("Enter the first number")
+    a =int(input())
+    print("Enter the second number")
+    b = int(input())
+    c = a + b
+    ans = "{} + {} = {}".format(a,b,c)
+    print(ans)
 
-
-
+math(a,b,c)
